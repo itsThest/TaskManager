85ccf61138cda0c990ca2eaafa76bb4d3b0816f1
Bixmarck Rodríguez <areb64areb64@gmail.com>
2026-09-02 06:09:50 -0500

    feat: agregar AppDbContext y migración inicial de EF Core

--- task-manager-backend/Program.cs
+++ task-manager-backend/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerBackend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
